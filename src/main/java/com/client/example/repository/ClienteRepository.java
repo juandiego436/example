5d937fa033fd16f5bc538346e552bc30be9029ec
Juan Diego Caceres Munares <44043720+juandiego436@users.noreply.github.com>
2022-01-10 09:49:31 -0500

    terminar cliente

--- src/main/java/com/client/example/repository/ClienteRepository.java
+++ src/main/java/com/client/example/repository/ClienteRepository.java
@@ -1,7 +1,6 @@
 package com.client.example.repository;
 
 import com.client.example.entity.Cliente;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
